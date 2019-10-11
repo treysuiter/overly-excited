cd880f74c7d7e3663eace31232d05ec63761f5dc
Trey Suiter <trey.suiter@gmail.com>
2019-10-10 21:16:31 -0500

    added argument to addExcitement to select punc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,7 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
     the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement(theWordArray) {
+function addExcitement(theWordArray, mark) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
@@ -16,7 +16,7 @@ function addExcitement(theWordArray) {
     for (let i = 0; i < theWordArray.length; i++) {
         // Concatenate the new word onto buildMeUp
         if ((i + 1) % 3 === 0) {
-            buildMeUp += " " + theWordArray[i] + "!"
+            buildMeUp += " " + theWordArray[i] + mark
         } else {
             buildMeUp += " " + theWordArray[i]
         }
@@ -27,4 +27,4 @@ function addExcitement(theWordArray) {
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence)
+addExcitement(sentence, "?")
